138dd9fca16bbc6e7ee8097645081bcd0eed3c7f
Lison <l.ferne@interface3.be>
2021-11-29 11:14:08 +0100

    images

--- view/html/head.php
+++ view/html/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lison Ferné | Junior Front-end développeuse & Illustratrice</title>
+    <title>Lison Ferné | Front-end développeuse Junior & Illustratrice</title>
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
