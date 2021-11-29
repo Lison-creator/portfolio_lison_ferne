3cad0f5e8c72e34e26cb22e1e27f6d99c160dc2a
Lison <l.ferne@interface3.be>
2021-11-29 10:44:51 +0100

    29 nov 11h

--- view/page/portfolio.php
+++ view/page/portfolio.php
@@ -1,18 +1,18 @@
 <!-- Structure:
 
-Div 1(100vh): contient le #portfolio artistique
+Section 1(100vh): contient le #portfolio artistique
     Float: 
         - div 1: bloc noir de contact
         - div 2: la ligne du temps avec les images (dans des div séparées)
     
-Div 2(100vh): Contient la page "#A propos" (en float avec la div 3 de la page "Formation")
+Section 2(100vh): Contient la page "#A propos" (en float avec la div 3 de la page "Formation")
     1 div pour chaque image-texte cliquable ("Contact", Art, Formation, Dev)
     1 div pour l'image profil
     1 div pour le panneau déroulant "Contact"
 
-Div 3(100vh): Page Formation (float avec la div "A propos")
+Section 3(100vh): Page Formation (float avec la div "A propos")
 
-Div 4: #Developpement Web
+Section 4: #Developpement Web
     2 div à 100vh chacune
         div 1: #Compétences
             1 div pour le panneau déroulant "Contact"
@@ -24,6 +24,110 @@ Div 4: #Developpement Web
             1 div blocs cliquables du portfolio
             1 div pour le fil d'ariane (à droite)
             1 div pour le lien vers la page "A propos"
+
+            href="#background"
     -->
 
-<h1>Coucou</h1>
+<h1>Coucou test</h1>
+
+<!-- --------------- PAGE PORTFOLIO ARTISTIQUE --------------- -->
+<section id="portfolioArtistique">
+    <!-- ////////////// Bloc noir de contact ////////////// -->
+
+    <div>
+    </div>
+
+    <!-- ////////////// FIN Bloc noir de contact ////////////// -->
+
+
+    <!-- ////////////// Les projets artistiques (ligne du temps) ////////////// -->
+
+    <div></div>
+
+    <!-- ////////////// FIN Les projets artistiques (ligne du temps) ////////////// -->
+
+</section>
+<!-- --------------- FIN PAGE PORTFOLIO ARTISTIQUE --------------- -->
+
+<!-- --------------- PAGE A PROPOS (bloc principal)--------------- -->
+
+<section id="aPropos">
+    <!-- div du haut (top et left à ajuster pour centrer): titre cliquable vers l'ancre "portfolio artistique" -->
+    <div>
+        <a href="#portfolioArtistique">ART</a>
+    </div>
+
+    <!-- div du milieu FLOAT (top et left à ajuster): Contact, "Lison Ferné" et image Lison + "Formation" -->
+    <div>
+        <div>
+            <div>
+                <a href="#contact">Contact</a>
+            </div>
+
+            <!-- Panneau déroulant au clic (js) -->
+            <div id="carteContact">
+                <!-- Les icones dans une div flottante -->
+                <div>Les icones</div>
+                <div>
+                    <a href="">Mail</a> <br>
+                    Née le 22 avril 1993
+                </div>
+                <div>Image poissons bottom</div>
+            </div>
+        </div>
+        <div>
+            <a href="#">Lison Ferné</a>
+        </div>
+        <div>
+            <a href="#formation">Formation + image</a>
+        </div>
+    </div>
+
+    <!-- div du bas "Développement Web" -->
+    <div>
+        <a href="#competences">Développement Web</a>
+    </div>
+</section>
+<!-- --------------- FIN PAGE A PROPOS (bloc principal)--------------- -->
+
+
+<!-- --------------- PAGE FORMATION (float avec "A propos") --------------- -->
+
+<section id="formation"></section>
+
+<!-- --------------- FIN PAGE FORMATION --------------- -->
+
+
+<!-- --------------- PAGE WEBDEV --------------- -->
+
+<section id="webdev">
+    <!-- ////////////// Compétences ////////////// -->
+
+    <div id="competences">
+        <div>
+            <!-- Float -->
+            <div>Panneau contact</div>
+            <div>Bloc des compétences</div>
+            <div>Fil d'ariane</div>
+        </div>
+        <div>
+            <a href="#portfolioWebDev">Portfolio</a>
+        </div>
+    </div>
+
+    <!-- ////////////// Portfolio WebDev ////////////// -->
+
+    <div id="portfolioWebDev">
+    <div>
+            <!-- Float -->
+            <div>Menu des languages de programmation</div>
+            <div>Bloc portfolio</div>
+            <div>Fil d'ariane</div>
+        </div>
+        <div>
+            <a href="#aPropos">à propos</a>
+        </div>
+    </div>
+</section>
+
+<!-- --------------- FIN PAGE WEBDEV --------------- -->
