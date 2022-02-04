a9aac94bd5f8d3e79029afc09f23c08b9cffe4a7
Lison <l.ferne@interface3.be>
2022-02-04 11:59:22 +0100

    favicon pour la barre de navigation

--- view/html/head.php
+++ view/html/head.php
@@ -7,6 +7,10 @@
     <title>Lison Ferné | Front-end développeuse Junior & Illustratrice</title>
     <link rel="stylesheet" href="public/css/style.css">
 
+	<!-- Icone de barre de navigation -->
+	<link rel="icon" type="image/x-icon" sizes="16x16" href="public/images/head/faviconLisonFerne.png">
+
+
     <!-- Animation Animate de la signature -->
     <!--  //TODO Debugger -->
     <script src="https://code.createjs.com/1.0.0/createjs.min.js"></script>
